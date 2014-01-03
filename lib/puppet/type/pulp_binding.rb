e63c6ed9caedd37a5b27d95c9e53e24c213b5aaf
Drew Blessing <drew.blessing@buckle.com>
2014-01-03 14:32:51 -0600

    Add minimal support

--- lib/puppet/type/pulp_binding.rb
+++ lib/puppet/type/pulp_binding.rb
@@ -12,10 +12,12 @@ Puppet::Type.newtype(:pulp_binding) do
     end
   end
 
+  newparam(:url)
   newparam(:login)
   newparam(:password)
 
   validate do
+    fail 'must specify url' if self[:url].to_s.empty?
     fail 'must specify login' if self[:login].to_s.empty?
     fail 'must specify password' if self[:password].to_s.empty?
   end
